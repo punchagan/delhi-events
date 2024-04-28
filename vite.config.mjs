85ceaca8c53d923f4747470ee5d9b0508457563a
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-04-28 12:06:09 +0530

    Setup the base field in the vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
   root: "_build/default/",
+  base: "/delhi-events/",
   publicDir: "static",
   plugins: [nodeResolve()],
   server: {
